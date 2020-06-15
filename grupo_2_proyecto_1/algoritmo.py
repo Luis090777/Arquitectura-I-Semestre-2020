8cc471244f844de8d1963dc94101d09c31c2dfe7
Marco <marcom4drigal@gmail.com>
2020-06-14 18:08:58 -0600

    Se arregla la funcion para verificar numeros

--- grupo_2_proyecto_1/algoritmo.py
+++ grupo_2_proyecto_1/algoritmo.py
@@ -1,38 +1,39 @@
 def validar_numeros(a,b):
 
-    if type(a) != int:
-        print("1111")
-        print("Ingrese solo números")
-        return False
+  """
+  Validates if a and b are numbers, if they aren't, the program stops
+  Input: a and b are numbers or text
+  Output: True if the a and b are numbers, False if a or/and b aren't numbers
+  """
 
-    elif type(b) != int:
-        
-        print("Ingrese solo números")
-        return False
+  if type(a) != int: #If a isn't a integer, return False
+    return False
 
-    elif a > 255 and b > 255:
-        return False
+  elif type(b) != int: #If b isn't a integer, return False
+    return False
 
-    else:
-        return True
+  elif a > 255 and b > 255: #If a or b are out of the number range of 8-bits, return False
+    return False
 
+  else: #If both are valid numbers, return True
+    return True
 
+    
 def main():
 
-    '''
-    We will use this if we have to ask for the numbers
-    a = int(input("intruduzca el primer numero a multiplicar: \n" ))
+  """
+  We will use this if we have to ask for the numbers
+  a = int(input("intruduzca el primer numero a multiplicar: \n" ))
 
-    b = int(input("intruduzca el segundo numero a multiplicar: \n"))
-    '''
-    a = 3
-    b = 6
+  b = int(input("intruduzca el segundo numero a multiplicar: \n"))
+  """
+  a = 3
+  b = 6
     
-    if validar_numeros(a,b) != True:
-        print("Ingrese solo números")
+  if (validar_numeros(a,b) != True): #Calls validar_numeros function
+  		print("Ingrese solo números")
     
-    else:
-        return None
-        # Aquí va la función de multiplicar
-
-print(main())
+  else: #Prints the result of the multiplicated numbers
+    print ""
+    
+main()
