fe64839892cd3a976efdeb518b67104067ff4590
Luis090777 <57786749+Luis090777@users.noreply.github.com>
2020-06-14 20:08:03 -0600

    Añado función  dec_a_bin y sumador_binario

--- grupo_2_proyecto_1/algoritmo.py
+++ grupo_2_proyecto_1/algoritmo.py
@@ -1,3 +1,53 @@
+def sumador_binario(numero_a, numero_b):  
+  """
+    Binary addition.
+    Input: numero_a and numero_b are a tuple with one number each.
+    Output: Will return the addition as a tuple
+ 
+  """
+
+  # Compare the size of the 2 tuples and add make them equal
+  tamanio = len(numero_a)-len(numero_b)
+
+  if tamanio > 0:
+        # Add 0's as necessary to equal the size of numbers
+    numero_b = ((0,) * tamanio) + numero_b
+        
+  elif tamanio < 0:
+        
+        #Add 0's as neccessary to equal the size of numbers
+    numero_a = ((0,) * (-tamanio)) + numero_a # We use "-tamanio" to convert tamanio to a positive number
+  
+  carry = 0
+  arreglo_bits = [0]*(len(numero_a)+1) # Final array with 0's and 1's
+
+  # Go through the array adding 0's or 1's according to the division or the multiplication
+  for j in reversed(range(0, len(numero_a))):
+
+    # Starts the cycle to add the bits of both of the tuples
+    # Add the numbers from the tuples and divide them by 2, to get 0 or 1 depending on the
+    # upcoming bits
+    division_entera = (numero_a[j] + numero_b[j] + carry) // 2
+
+    # Assign the previous operation to the current positions in the array, because we are
+    # adding bits, so we have to get 0's and 1's and in this case
+    # j+1 because the array has one more position than the tuples.
+    arreglo_bits[j+1] = (numero_a[j] + numero_b[j] + carry) - 2*division_entera
+
+    # Takes the value of division_entera to keep doing the
+    # multiplication correctly in the next iteration
+    carry = division_entera 
+                            
+          
+    arreglo_bits[0] = carry # Assign the carry to the first position of the array, because                          # it's a carry
+  
+
+  if arreglo_bits[0] == 0: #When arreglo_bits doesn't increase the bits of the original numbers, eliminates the extra 0
+
+    arreglo_bits.remove(0) 
+  return tuple(arreglo_bits)
+
+
 def corrimiento_izquierda(numero_a,bits):
   """
   Make a shift left of the upcoming bits by n bits.
@@ -16,7 +66,26 @@ def corrimiento_izquierda(numero_a,bits):
   # we are using a step by step operation to add 0's to the tuple
   else:
     return numero_a + (0,) * bits
+  
+  
+def dec_a_bin(n):
 
+  """
+  Converts the given number from decimal to binary
+  Input: n is a base 10 number
+  Output: A string with the binary number
+  """
+  
+  
+  if n < 0: #If the number is bigger than 0
+    return "Debe ser un número mayor a 0"
+    
+  elif n == 0: #If the number is equal to zero
+    return "0"
+    
+  else: #If n is a valid number, add the 0 or 1 at the verified position and call this function again
+    return dec_a_bin(n//2) + str(n%2)
+        
 def validar_numeros(a,b):
 
   """
